0c0776702621bde25755b557b42cf4b43375d035
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2024-03-10 22:03:48 -0500

    adding dark mode switch

--- tests/Officify.Web.Host.Tests/Shell/AppBarTests.cs
+++ tests/Officify.Web.Host.Tests/Shell/AppBarTests.cs
@@ -1,4 +1,4 @@
-using Bunit;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Officify.Web.Host.Shell;
 
@@ -16,6 +16,22 @@ public class AppBarTests : OfficifyTestContext
         });
 
         await appBar.Find("button").ClickAsync(new MouseEventArgs());
+
+        toggleCount.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task WhenToggleDarkModeThenNotifiesToToggleDarkMode()
+    {
+        var toggleCount = 0;
+        var appBar = RenderComponent<AppBar>(param =>
+        {
+            param.Add(a => a.OnToggleDarkMode, () => toggleCount++).Add(a => a.IsDarkMode, false);
+        });
+
+        await appBar
+            .Find("input[type=\"checkbox\"]")
+            .ChangeAsync(new ChangeEventArgs { Value = true });
 
         toggleCount.Should().Be(1);
     }
