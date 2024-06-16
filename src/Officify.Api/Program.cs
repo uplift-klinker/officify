036dcf06b83db48e703776495195902b426176f4
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2024-06-15 22:31:55 -0500

    application runs properly

--- src/Officify.Api/Program.cs
+++ src/Officify.Api/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- src/Officify.AppHost/Program.cs
+++ src/Officify.AppHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiService = builder.AddProject<Projects.Officify_Api>("api");
+
+builder.AddProject<Projects.Officify_Web>("web")
+    .WithReference(apiService);
 builder.Build().Run();
